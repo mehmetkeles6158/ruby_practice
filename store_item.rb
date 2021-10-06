a1cec83896b93705fefcfc269141f7fa361299f0
Mehmet <mehmetk6158@gmail.com>
2021-10-05 21:56:58 -0400

    store_item with hash inputs

--- store_item.rb
+++ store_item.rb
@@ -7,47 +7,53 @@
 
 class Item
 
-  #getter/reader methods
+  # def item_id
+  #   @item_id
+  # end
 
-  def item_id
-    @item_id
-  end
+  # def item_name
+  #   @item_name
+  # end
 
-  def item_name
-    @item_name
-  end
+  # def item_price
+  #   @item_price
+  # end
 
-  def item_price
-    @item_price
-  end
-
-  def item_stock
-    @item_stock
-  end
+  # def item_stock
+  #   @item_stock
+  # end
 
   # setter/ writer methods
 
-  def item_id=(i)
-    @item_id = i
-  end
+  # def item_id=(i)
+  #   @item_id = i
+  # end
 
-  def item_name=(n)
-    @item_id = n 
-  end
+  # def item_name=(n)
+  #   @item_id = n 
+  # end
 
-  def item_price=(p)
-    @item_id = p
-  end
+  # def item_price=(p)
+  #   @item_id = p
+  # end
 
-  def item_stock=(s)
-    @item_id = s
-  end
+  # def item_stock=(s)
+  #   @item_id = s
+  # end
 
-  def initialize(input_item_id, input_item_name, input_item_price, input_item_stock)
-    @item_id = input_item_id
-    @item_name = input_item_name
-    @item_price = input_item_price
-    @item_stock = input_item_stock
+
+  #getter/reader methods
+
+  attr_reader :item_id, :item_name, :item_price, :item_stock
+  #setter/writer methods
+  attr_writer :item_id, :item_name, :item_price, :item_stock
+
+
+  def initialize(input_options)
+    @item_id = input_options[:item_id]
+    @item_name = input_options[:item_name]
+    @item_price = input_options[:item_price]
+    @item_stock = input_options[:item_stock]
   end
 
   def print_info
@@ -55,14 +61,23 @@ class Item
   end
 
   def tax_price
+    @item_price *= 1.08
   end
 
 end
 
-item1 = Item.new(11, "IZZE", 1.99 , true)
+item1 = Item.new(item_id: 11, item_name: "IZZE" , item_price:1.99 , item_stock: true )
 
 item1.print_info
 
+item1.item_id = 22
 
+p item1.item_id
+
+item1.print_info
+
+item1.item_price = 2.99
+
+item1.print_info
 
 
