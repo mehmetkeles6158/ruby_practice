6caa30465a33e80bdbed5f581e9daffb4f93cc5d
Mehmet <mehmetk6158@gmail.com>
2021-11-25 20:32:43 -0500

    commit some changes

---
+++ take_home_assignments.rb
@@ -0,0 +1,21 @@
+# Solve an algorithm challenge with given inputs/outputs and/or automated tests
+# Sequential Bi-grams
+# Given a string of words, create a set of every sequential bi-gram.  Bigrams are two-word combinations of the text, i.e. "Charlie rocks".
+ 
+# Example
+# "Make a killer impression on whoever you're meeting."
+ 
+# returns
+# "Make a"
+# "a killer"
+# "killer impression"
+# "impression on"
+# Etc.
+
+# requirements
+# Generates a set of every possible bigram from a string of text
+# Keep performance & memory usage in mind
+# Include tests
+# We prefer Ruby or Python, but you may use any language.
+
+string = "Make a killer impression on whoever you're meeting."
