0ae6f7c0644d4859a04b0e6fb0f743cdffb54f56
Mehmet <mehmetk6158@gmail.com>
2022-01-08 15:27:26 -0500

    find number in an array

--- hackerrank.rb
+++ hackerrank.rb
@@ -1,29 +1,29 @@
-def breakingRecords(scores)
-  # Write your code here
-  solution = []
-  highest = scores[0]
-  lowest = scores[0]
-  count1 = 0
-  count2 = 0
-  i = 1
-  while i < scores.length
-      if scores[i] > highest
-        highest = scores[i]
-        count1 += 1
-      elsif scores[i] < lowest
-        lowest = scores[i]
-        count2 += 1
-      end
-      i+= 1
-  end
-  solution << count1
-  solution << count2
-  p solution
+# def breakingRecords(scores)
+#   # Write your code here
+#   solution = []
+#   highest = scores[0]
+#   lowest = scores[0]
+#   count1 = 0
+#   count2 = 0
+#   i = 1
+#   while i < scores.length
+#       if scores[i] > highest
+#         highest = scores[i]
+#         count1 += 1
+#       elsif scores[i] < lowest
+#         lowest = scores[i]
+#         count2 += 1
+#       end
+#       i+= 1
+#   end
+#   solution << count1
+#   solution << count2
+#   p solution
 
-end
+# end
 
-s = [10, 5, 20, 20, 4, 5, 2, 25, 1]
-p breakingRecords(s)
+# s = [10, 5, 20, 20, 4, 5, 2, 25, 1]
+# p breakingRecords(s)
 
 #--------------------------------------------------
 
@@ -54,25 +54,41 @@ p breakingRecords(s)
 
 # int: the number of ways the bar can be divided
 
-def birthday(s, d, m)
+# def birthday(s, d, m)
+#   # Write your code here
+#   count = 0
+#   if s.length == 1
+#     count = 1
+#   end
+#   i = 0 
+#   while i < s.length-1
+#     if s[i] + s[i+1] == d
+#       count += 1
+#     end
+#     i+=1
+#   end
+#  count
+# end
+
+# s= [1, 2, 1, 3, 2]
+# d=3 
+# m=2
+
+# p birthday(s, d, m)
+
+# --------Find a number------------#
+def findNumber(arr, k)
   # Write your code here
-  count = 0
-  if s.length == 1
-    count = 1
-  end
-  i = 0 
-  while i < s.length-1
-    if s[i] + s[i+1] == d
-      count += 1
+  
+  arr.each do |num|
+    if k == num
+      return "YES"
     end
-    i+=1
+    if k != num
+      return "NO"
+    end
   end
- count
+
 end
 
-s= [1, 2, 1, 3, 2]
-d=3 
-m=2
-
-p birthday(s, d, m)
-
+findNumber([1,2,3],2)
