282f2e55643008d568e129ffe962c2356a6c39e9
Mehmet <mehmetk6158@gmail.com>
2022-01-13 09:04:46 -0500

    checking a palindrome

--- practice-11.rb
+++ practice-11.rb
@@ -1,15 +1,33 @@
 # 
 
-def great_word(n)
-  if n % 3 == 0 && n % 5 == 0
-    p "FizzBuzz"
-  elsif n % 3 == 0
-    p "Fizz"
-  elsif n % 5 == 0
-    p "Buzz"
-  else
-    puts n
+# def great_word(n)
+#   if n % 3 == 0 && n % 5 == 0
+#     p "FizzBuzz"
+#   elsif n % 3 == 0
+#     p "Fizz"
+#   elsif n % 5 == 0
+#     p "Buzz"
+#   else
+#     puts n
+#   end
+# end
+
+# great_word(4)
+
+
+def palindrome(s)
+  i = 0
+  j = s.length - 1
+  while i < j
+    if s[i] != s[j]
+      return false
+    else
+      return true
+    end
+
+    i += 1
+    j -= 1
   end
 end
 
-great_word(4)
+p palindrome("racecar")
